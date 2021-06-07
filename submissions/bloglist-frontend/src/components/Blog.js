efbd897d63e5c91a27a1ecb56b1d2f6c11c35433
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-06-06 18:52:48 -0700

    Ex 5.7: bloglist frontend, step 7. Added button to each Blog component that shows/hides full details of blog upon click.

--- submissions/bloglist-frontend/src/components/Blog.js
+++ submissions/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,45 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ( {blog} ) => {
+  const [ visible, setVisible ] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : ''}
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = event => setVisible(!visible)
+  
+
+  const like = (event) => {
+    console.log(event.target, 'clicked')
+  }
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <pre>
+          {blog.title} {blog.author}
+          {'\n'}{blog.url}
+          {'\n'}{blog.likes}<button onClick={like}>like</button>
+          {'\n'}{blog.user.name}
+        </pre>
+        <button onClick={toggleVisibility}>hide</button>
+     
+      </div>
+    </div>
+  )
+ 
+}
 
 export default Blog
