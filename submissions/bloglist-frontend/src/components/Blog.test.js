0a9f0639a20e23982b5aafe49d1bef9761f91abd
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-06-09 20:29:46 -0700

    Ex 5.13: blog list tests, step 2. Implemented test which checks that blog url, number of likes, and name of user who created are shown when the button controlling the shown details has been clicked

--- submissions/bloglist-frontend/src/components/Blog.test.js
+++ submissions/bloglist-frontend/src/components/Blog.test.js
@@ -35,3 +35,44 @@ test('by default, only title and author of a blog are rendered', () => {
     const divDefaultHide = component.container.querySelector('.defaultHideContent')
     expect(divDefaultHide).toHaveStyle('display: none')
 })
+
+test('when button controlling shown details is clicked, blog url and number of likes are shown', () => {
+    const blog = {
+        author: 'Test author',
+        id: 'blogID6abbcd',
+        likes: 1,
+        title: 'Test blog object for testing default render behaviour',
+        url: 'link.com',
+        user: {
+            id: 'userID60b93e',
+            name: 'Testuser',
+            username: 'test'
+        }
+    }
+
+    const component = render(
+        <Blog blog={blog} />
+    )
+
+    const buttonView = component.getByText('view')
+    expect(buttonView).toBeDefined()
+    fireEvent.click(buttonView)
+
+    const divDefaultShow = component.container.querySelector('.defaultShowContent')
+    expect(divDefaultShow).toHaveStyle('display: none')
+    const divDefaultHide = component.container.querySelector('.defaultHideContent')
+    expect(divDefaultHide).not.toHaveStyle('display: none')
+    expect(divDefaultHide).toHaveTextContent(
+        'Test author'
+    )
+    expect(divDefaultHide).toHaveTextContent(
+        'Test blog object for testing default render behaviour'
+    )
+    expect(divDefaultHide).toHaveTextContent(
+        'likes: 1'
+    )
+    expect(divDefaultHide).toHaveTextContent(
+        'Testuser'
+    )
+
+})
